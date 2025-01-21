8aa3f349f198d1351752c482c7cdc3da613c6d96
Rakha <rkhdinar00@gmail.com>
2025-01-21 18:47:03 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+// TEST GITHUB
 
 require __DIR__ . '/auth.php';
